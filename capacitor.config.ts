b06a78dd3653628a7694fb790ec9d8d8c4024594
Ian <fyanmahendra@gmail.com>
2024-06-07 12:24:12 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tugas-crypto',
+  webDir: 'dist'
+};
+
+export default config;
